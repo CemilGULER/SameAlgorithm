eb002956e3435c5f4d94d144836e751949f5a560
CemilGULER <78479096+CemilGULER@users.noreply.github.com>
2022-12-20 16:16:04 +0300

    Update Program.cs

--- Test/Program.cs
+++ Test/Program.cs
@@ -138,6 +138,23 @@ for(int i = 0; i < myNum.Length; i++)
 }
         
         ////////
+        
+    ///Armstrong    
+        int n, r, sum = 0, temp;
+Console.Write("Enter the Number= ");
+n = int.Parse(Console.ReadLine());
+temp = n;
+while (n > 0)
+{
+    r = n % 10;
+    sum = sum + (r * r * r);
+    n = n / 10;
+}
+if (temp == sum)
+    Console.Write("Armstrong Number.");
+else
+    Console.Write("Not Armstrong Number.");
+        ////////////////////
         static bool ValidatePerBraces(char a, char b)
         {
 
